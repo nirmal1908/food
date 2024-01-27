27264c7d4cc9d56976d0b1314894cf1ce53dd79c
nirmal1908 <157907730+nirmal1908@users.noreply.github.com>
2024-01-27 23:02:27 +0530

    added index

---
+++ food.js
@@ -0,0 +1,9 @@
+function orderNow(itemName) {
+    // Add logic here for ordering the selected item
+    alert('Order placed for ' + itemName);
+  }
+  function AddCart(itemName) {
+    // Add logic here for ordering the selected item
+    alert('Add Food In Bag ' + itemName);
+  }
+  
